a429c404f0863bab3ac6299d1dd73c7f39e7cb9c
Hadeel Hassan <hadeelhassan186@gmail.com>
2023-01-02 10:01:02 +0200

    fixed solution

--- hw3/hw3 solution.py
+++ hw3/hw3 solution.py
@@ -1,77 +1,78 @@
 # exercise1
-#
-# Mydic = {('name, Last_name'): 'Hadeel Hassan', 'age': '31', 'PhoneNumber': '0525787713'}
-# print(Mydic)
-#
+
+Mydic = {('name, Last_name'): 'Hadeel Hassan', 'age': '31', 'PhoneNumber': '0525787713'}
+print(Mydic)
+
 # exercise2
 
-# def replace(mylist):
-#     try:
-#         len(mylist) > 5
-#     except:
-#         print("Your list is too short!")
-#     else:
-#         mylist[5] = "@"
-#         return mylist
-#
-#
-# x= replace([1,2,3,4,5,6,7,8,9])
-# print(x)
+def func(mylist):
+    try:
+        if len(mylist) > 5:
+            mylist[5] = "@"
+    except IndexError:
+        print("Your list is too short!")
+    else:
+        return mylist
+
+
+func([1,2,3,4,5,6,7,8,9])
+print(func([1,2,3,4,5,6,7,8,9]))
 
 # exercise3
 
-# def func(mylist):
-#     assert len(mylist) > 5, "Your list is too short!"
-#     mylist[5] = '@'
-#     return mylist
-#
-#
-# func([1,2,3,4,5,6,7,8,9])
-# print(func([1,2,3,4,5,6,7,8,9]))
+def func(mylist):
+    assert len(mylist) > 5, "Your list is too short!"
+    mylist[5] = '@'
+    return mylist
+
+
+func([1,2,3,4,5,6,7,8,9])
+print(func([1,2,3,4,5,6,7,8,9]))
 
 # exercise4
 
-# def func(dict1, dict2):
-#     dict1.update(dict2)
-#     dict2.update(dict1)
-#     return (dict1)
-#
-# dict1 = {'key1': 'Hello', 'key2': 'World'}
-# dict2 = {'key3': 'Python'}
-# print (func(dict1, dict2))
-# #
+def func(dict1, dict2):
+    dict1.update(dict2)
+    dict2.update(dict1)
+    return (dict1)
+
+dict1 = {'key1': 'Hello', 'key2': 'World'}
+dict2 = {'key3': 'Python'}
+print (func(dict1, dict2))
+
 # exercise5
 
-# dict = {}
-# n = 5
-# for x in range(1,n+1):
-#     dict[x]=x+3
-# print(dict)
+dict = {}
+n = 5
+for x in range(1,n+1):
+    dict[x]=x+3
+print(dict)
 
 # exercise6
-# dict1 = {1:10, 2:20}
-# dict2 = {3:30, 4:40}
-# dict3 = {5:50, 6:60}
-# dict4 = {}
-# for x in (dict1, dict2, dict3):
-#     dict4.update(x)
-# print(dict4)
+
+dict1 = {1:10, 2:20}
+dict2 = {3:30, 4:40}
+dict3 = {5:50, 6:60}
+dict4 = {}
+for x in (dict1, dict2, dict3):
+    dict4.update(x)
+print(dict4)
 
 # exercise7
 
-# dict1 = {}
-# def func(mystr):
-#     for char in mystr:
-#         count = dict1.get(char, 0)
-#         count += 1
-#         dict1[char] = count
-#     chars = dict1.keys()
-#     chars = sorted(chars)
-# func("hadeelhassan")
-# print(dict1)
+dict1 = {}
+def func(mystr):
+    for char in mystr:
+        count = dict1.get(char, 0)
+        count += 1
+        dict1[char] = count
+    chars = dict1.keys()
+    chars = sorted(chars)
+func("hadeelhassan")
+print(dict1)
 
 #  exercise8
-#
+
 
 
 def func(dict1, dict2):
@@ -89,3 +90,4 @@ def func(dict1, dict2):
 dict1 = {'a': 1, 'b': 2, 'c': 3}
 dict2 = {'a': 4, 'e': 5, 'c': 6}
 print(func(dict1, dict2))
+
